672f1c039dda763579389de4258eed2618ac2a37
Hawiana Abebe <iamhawiana@gmail.com>
2023-09-29 06:40:51 +0300

    Update form

--- form.php
+++ form.php
@@ -1,9 +1,9 @@
 <?php
-    require 'constants.php';
+    require 'header.php';
  ?>
  
  <h2> Register Writer </h2>
-    <form action="insert.php" method="POST">
+    <form action="processes.php" method="POST">
         <label for="writer_id">Writer ID:</label>
         <input type="text" name="writer_id" required autocomplete="off"><br><br>
 
